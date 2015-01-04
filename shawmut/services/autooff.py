12ebb8d2444c374ec3aa0ca987863e4e18e8de6d
Stephanie <sknopf@twitter.com>
2015-01-03 19:01:49 -0500

    Added -q/--quiet option

--- shawmut/services/autooff.py
+++ shawmut/services/autooff.py
@@ -6,6 +6,7 @@ import json
 import os.path
 import time
 from datetime import datetime
+from optparse import OptionParser
 from shawmut.settings import conf
 from shawmut.weather import ShawmutWeather
 
@@ -13,12 +14,17 @@ SCHEDULER_INTERVAL = 5
 CONNECTION_TIMEOUT = 10
 
 class AutoOffPoller(object):
-    def __init__(self):
+    def __init__(self, verbose=False):
         self.is_away = False
         self.weather = ShawmutWeather()
+        self.verbose = verbose
 
     def log(self, msg):
         print "%s: %s" % (datetime.now().strftime('%Y-%m-%d %H:%M:%S'), msg)
+
+    def log_debug(self, msg):
+        if self.verbose:
+            self.log(msg)
 
     def check_if_home(self):
         for ip in conf.iphone_ips:
@@ -26,13 +32,13 @@ class AutoOffPoller(object):
                 conn = socket.create_connection((ip, 62078), CONNECTION_TIMEOUT)
                 return True
             except socket.timeout:
-                self.log("Timed out: %s is not connected to our network" % ip)
+                self.log_debug("Timed out: %s is not connected to our network" % ip)
         return False
 
-    def have_guests(self):
-        os.path.isfile('/etc/shawmut/have_guests')
+    def has_guests(self):
+        os.path.isfile('/etc/shawmut/has_guests')
 
-    def light_data(self):
+    def get_light_data(self):
         try:
             data = urllib2.urlopen('http://localhost:5000/api/environment').read()
             return json.loads(data)
@@ -41,13 +47,13 @@ class AutoOffPoller(object):
             return {}
 
     def off_lights(self):
-        return list(name for (name, data) in self.light_data().iteritems() if data['state'] == 0)
+        return list(name for (name, data) in self.get_light_data().iteritems() if data['state'] == 0)
 
     def on_lights(self):
-        return list(name for (name, data) in self.light_data().iteritems() if data['state'] == 1)
+        return list(name for (name, data) in self.get_light_data().iteritems() if data['state'] == 1)
 
     def toggle_lights(self, lights):
-        # Race condition if someone turns on lights manually between finding off/on lights and toggling/
+        # Race condition if someone turns on lights manually between finding off/on lights and toggling
         for l in lights:
             try:
                 urllib2.urlopen("http://localhost:5000/api/device/%s" %l, '{"state":"toggle"}')
@@ -67,26 +73,32 @@ class AutoOffPoller(object):
             self.toggle_lights(found_on_lights)
 
     def poll(self):
-        self.log("Starting. is_away set to %s" % self.is_away)
+        self.log_debug("Starting poll - is_away set to %s" % self.is_away)
         is_home = self.check_if_home()
 
-        if self.have_guests():
+        if self.has_guests():
             next
         elif is_home and self.is_away:
-            self.log('Home james: Turning on any off lights and setting self.is_away to False')
+            self.log_debug('Home james: Turning on any off lights and setting self.is_away to False')
             self.turn_on_lights()
             self.is_away = False
         elif not is_home and not self.is_away:
-            self.log('Gonzo: setting self.is_away to True and cheking if it\'s dark out')
+            self.log_debug('Gonzo: Setting self.is_away to True and cheking if it\'s dark out')
             self.is_away = True
             if self.weather.is_dark():
-                self.log('It\s nightime: Turning off any on lights')
+                self.log_debug('It\s dark out: Turning off any on lights')
                 self.turn_off_lights()
         else:
-            self.log('No changes, doing nothing')
+            self.log_debug('No changes, doing nothing')
 
 def main():
-    auto_off = AutoOffPoller()
+    parser = OptionParser()
+    parser.add_option("-q", "--quiet",
+                      action="store_false", dest="verbose", default=True,
+                      help='Don\t print verbose output')
+    (options, args) = parser.parse_args()
+
+    auto_off = AutoOffPoller(options.verbose)
 
     while True:
         auto_off.poll()
